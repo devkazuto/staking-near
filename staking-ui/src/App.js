a48b5ad508e704137f9284faf7b3e61662cc3ab1
mb-1208 <mbntngfadlillahh1212@gmail.com>
2022-07-07 10:43:48 -0700

    ui

--- staking-ui/src/App.js
+++ staking-ui/src/App.js
@@ -1,22 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [walletName, setWalletName] = useState("");
+  const [tokenId, setTokenId] = useState(0);
+
+  useEffect(() => {
+    console.log(walletName);
+    console.log(tokenId);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">
+            Wallet User
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder="user.testnet"
+            onChange={(e) => {
+              setWalletName(e.target.value);
+            }}
+          />
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">
+            Token ID 0
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder="0"
+            onChange={(e) => {
+              setTokenId(e.target.value);
+            }}
+          />
+        </div>
+        <div class="row">
+          <div class="col">
+            <button type="button" class="btn btn-light">
+              Stake
+            </button>
+          </div>
+          <div class="col">
+            <button type="button" class="btn btn-light">
+              Claim
+            </button>
+          </div>
+          <div class="col">
+            <button type="button" class="btn btn-light">
+              Unstake
+            </button>
+          </div>
+        </div>
       </header>
     </div>
   );
